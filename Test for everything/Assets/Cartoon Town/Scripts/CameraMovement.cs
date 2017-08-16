c4bd70643f2bb240823fc4fd88fc7829db374a0b
Skiecode <30536311+Skiecode@users.noreply.github.com>
2017-08-16 12:27:00 +0200

    Added the  project
    Just added the simple project

---
+++ Test for everything/Assets/Cartoon Town/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public float _CameraMovementSpeed;
+	// Use this for initialization
+	void Start () {
+        _CameraMovementSpeed = _CameraMovementSpeed * -1;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            var _mousePos = Input.mousePosition;
+            _mousePos.x -= Screen.width / 2;
+            _mousePos.y -= Screen.height / 2;
+            _mousePos.Normalize();
+            Vector3 _dragDirection = new Vector3(_mousePos.x, 0, _mousePos.y) * _CameraMovementSpeed;
+            transform.position += _dragDirection;
+        }
+    }
+}
---
+++ Test for everything/Assets/Scripts/Unit.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+public class Unit : MonoBehaviour
+{
+
+    public Grid grid;
+    public Transform target;
+    public int layerIndex;
+    public float speed;
+
+    Vector3[] path;
+    List<Transform> unwalkable = new List<Transform>();
+    int targetIndex;
+    bool isWalking = false;
+    public bool hasWayChanged = true;
+
+    /// <summary>
+    /// Die FindObjectsOnLayer scheint nicht zu funken
+    /// </summary>
+
+    void Awake()
+    {
+        unwalkable = FindObjectsOnLayer(layerIndex);
+        for (int i = 0; i < unwalkable.Count; i++)
+        {
+            unwalkable[i].hasChanged = false;
+        }
+        StartCoroutine(CheckIfObjectsHaveChanged());
+    }
+
+    IEnumerator CheckIfObjectsHaveChanged()
+    {
+        while (true)
+        {
+            print("Checking if objects have changed...");
+            for (int i = 0; i < unwalkable.Count; i++)
+            {
+                if (unwalkable[i].hasChanged)
+                {
+                    print("Object " + unwalkable[i].name + " have changed");
+                    unwalkable[i].hasChanged = false;
+                    hasWayChanged = true;
+                    grid.SetupGrid();
+                    break;
+                }
+            }
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+
+    void Start()
+    {
+        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+    }
+
+    List<Transform> FindObjectsOnLayer(int layer)
+    {
+        GameObject[] goArray = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        List<Transform> goList = new List<Transform>();
+        foreach (GameObject go in goArray)
+        {
+            if (go.layer == layer)
+            {
+                goList.Add(go.transform);
+            }
+        }
+        return goList;
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+        else
+        {
+            print("Request new path");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0];
+        isWalking = true;
+        while (true)
+        {
+            if (!hasWayChanged)
+            {
+                if (transform.position == currentWaypoint)
+                {
+                    targetIndex++;
+                    if (targetIndex >= path.Length)
+                    {
+                        yield break;
+                    }
+                    currentWaypoint = path[targetIndex];
+                    this.transform.LookAt(path[targetIndex]);
+                }
+                transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+                yield return null;
+            }
+            else
+            {
+                print("Requesting new path " + gameObject.name);
+                PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+                hasWayChanged = false;
+                break;
+            }
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            for (int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(path[i], Vector3.one);
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
+    }
+}
